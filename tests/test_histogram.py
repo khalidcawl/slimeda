41b1797b21dc71355509f62e9ab86b2c4a1357f4
khalidcawl <khalidca@student.ubc.ca>
2022-01-19 15:48:17 -0800

    Test docstrings added

--- tests/test_histogram.py
+++ tests/test_histogram.py
@@ -4,6 +4,7 @@ from slimeda.histogram import histogram
 import pytest
 
 def test_histogram_single_chart():
+    """Tests that a single chart is created for a single column"""
     rng = np.random.default_rng()
 
     # create a dummy data frame with four columns for testing
@@ -16,6 +17,7 @@ def test_histogram_single_chart():
     assert charts[0].mark == "bar"
 
 def test_histogram_multiple_charts():
+    """Tests that multiple charts are created for multiple columns"""
     rng = np.random.default_rng()
 
     # create a dummy data frame with four columns for testing
@@ -29,16 +31,19 @@ def test_histogram_multiple_charts():
         assert chart.mark == "bar"
 
 def test_histogram_invalid_dataframe():
+    """Tests that invalid dataframe throws an error"""
     with pytest.raises(TypeError):
         histogram([1,2,3,4])
 
 def test_histogram_no_columns():
+    """Tests that function throws an error when no columns are specified"""
     with pytest.raises(ValueError):
         rng = np.random.default_rng()
         df = pd.DataFrame(rng.integers(0, 100, size=(100, 4)), columns=list('ABCD'))
         histogram(df, [])
 
 def test_histogram_wrong_column():
+    """Tests that function throws an error when user specifies an incorrect column name"""
     with pytest.raises(ValueError):
         rng = np.random.default_rng()
         df = pd.DataFrame(rng.integers(0, 100, size=(100, 4)), columns=list('ABCD'))
