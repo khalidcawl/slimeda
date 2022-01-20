97bf9af61fa8a975b15ad55012184a2e73800af3
thayehas <thayehas@gmail.com>
2022-01-19 18:33:10 -0800

    include docstring

--- tests/test_cat_unique_counts.py
+++ tests/test_cat_unique_counts.py
@@ -18,32 +18,12 @@ def df_sample():
     animal = ['rhino', 'cat', 'bufaloo', 'iguana']
     animal_col = np.random.choice(animal, 10 , p=[0.5, 0.1, 0.1, 0.3])
 
-    #generate random string date type
+    # generate random string date type
     str_date  = ['2021-01-01', '2021-01-05', '2021-01-25', '2021-01-15']
     str_date_col = np.random.choice(str_date, 10 , p=[0.5, 0.1, 0.1, 0.3])
 
     df = pd.DataFrame(data = [date_col, numbers_col, animal_col, str_date_col])
-    return df.T
 
-# bad input
-@pytest.mark.parametrize(
-    "bad_input", 
-    [
-        pd.Series(data= {'a': 1, 'b': 2, 'c': 3}, index=['x', 'y', 'z']),
-        np.array([[1, 2], [3, 4]]),
-        "folorunsho is a girl",
-        [3, 5, 9, 11],
-        9,
-        df_sample()
-    ]
-)
-def test_cat_unique_counts_bad_input(bad_input):
-    """Accepts parameters and rasie errors when parameter is of wrong instance"""
-    with pytest.raises(TypeError):
-        cat_unique_counts(bad_input)
-
-def test_cat_unique_counts():
-    """Unit test for missing value counts."""
     df1 = pd.DataFrame(data = [['Jessica',10,"cook"],
                         ['Tom',12,"swim"],
                         ['Clarke',1,"football"]], 
@@ -54,9 +34,41 @@ def test_cat_unique_counts():
                                 ['Clarke', 1, '4']], 
                            columns=['Name','Age', "Rate"])
 
+    df3 = pd.DataFrame( columns=['Name','Age', "Rate"])
+
+    return (df.T,df1, df2, df3)
+
+def test_cat_unique_counts():
+    """Unit test for unique counts of values in categorical features."""
+
+    df1 = df_sample()[1]
+    df2 = df_sample()[2]
+    df3 = df_sample()[3]
+
     result1 = list(cat_unique_counts(df1).iloc[:, 0])
     result2 = list(cat_unique_counts(df2).iloc[:, 0])
+    result3 = cat_unique_counts(df3)
 
     assert result1 == ['Name', 'Hobby']
     assert result2 == ['Name']
+    assert result3 == None
+    
+# bad input
+@pytest.mark.parametrize(
+    "input", 
+    [
+        pd.Series(data= {'a': 1, 'b': 2, 'c': 3}, index=['x', 'y', 'z']),
+        np.array([[1, 2], [3, 4]]),
+        "folorunsho is a girl",
+        [3, 5, 9, 11],
+        9,
+        df_sample()
+    ]
+)
+def test_cat_unique_counts_bad_input(bad_input):
+    """Accepts parameters and raise errors when parameter is of wrong instance"""
+    with pytest.raises(TypeError):
+        cat_unique_counts(input)
+
+
 
